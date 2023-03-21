2f0deeed6c3db4ebfda445df455f6218604ba25a
Hirwa Arnold <hategekimanahirwaarnold@gmail.com>
2023-03-21 03:45:27 +0200

    solve again

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const authCont = require('../controllers/authCont.js');
 const urouter = express.Router();
-const User = require('../models/User');
+const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const { requireAuth, requireSwagger } = require('../midddleware/authmiddleware'); 
